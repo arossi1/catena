d750321caedcc75b8fe3822ea85bf75d6f5bb6bb
arossi1@gmail.com <arossi1@gmail.com@741b4108-eae3-c65b-5bbe-89a607054baa>
2014-01-18 19:02:39 +0000

    cleaning up test/example scripts

--- code/Testing/persistence.py
+++ code/Testing/persistence.py
@@ -5,55 +5,34 @@ import Chain # Chain must be imported first, requirement of registry
 import Sources, FeatureExtraction, FeatureMatch, BundleAdjustment, Cluster
 
 
-def sfmChainProgrammatic(chainFilePath):
-    
-    # path to images
-    imagePath = "ETsub"
-    
-    # path to converted pgm images
-    imagePathPgm = os.path.join(imagePath,"pgm")
+def sfmChainBuild(chainFilePath, imagePath):
 
-    # PMVS path
-    pmvsPath = os.path.join(imagePath,"pmvs")
-
-    # log file / whether to parse keypoint descriptors
-    logPath = "log.txt"
-    parseKDF = False
-    
     # build chain
     imageSource = Sources.ImageSource(imagePath, "jpg")
-    imageConvert = Sources.ImageConvert(imageSource, imagePathPgm, "pgm")
-    sift = FeatureExtraction.Sift(imageConvert, parseKDF, "SiftHess")
-    keyMatch = FeatureMatch.KeyMatch(sift, parseKDF, "KeyMatchFull")
-    bundler = BundleAdjustment.Bundler([keyMatch, imageConvert])
+    sift = FeatureExtraction.Sift(imageSource, False, "SiftHess")
+    keyMatch = FeatureMatch.KeyMatch(sift, False, "KeyMatchFull")
+    bundler = BundleAdjustment.Bundler([keyMatch, imageSource])
     radialUndistort = Cluster.RadialUndistort([bundler, imageSource])
-    prepCmvsPmvs = Cluster.PrepCmvsPmvs(radialUndistort, pmvsPath)
+    prepCmvsPmvs = Cluster.PrepCmvsPmvs(radialUndistort, os.path.join(imagePath,"pmvs"))
     cmvs = Cluster.CMVS(prepCmvsPmvs)
     pmvs = Cluster.PMVS(cmvs)
-    
-    # render chain
-    #print Chain.Render(pmvs,logPath)
-    
+
     # persist chain
     Chain.StageRegistry.Save(chainFilePath)
     
     
-def sfmChainPersist(chainFilePath):
-    
-    # log file
-    logPath = "log.txt"
+def sfmChainRestoreRender(chainFilePath):
     
     # load the sfm chain
     headStages, tailStages = Chain.StageRegistry.Load(chainFilePath)
 
     # render the tail stage (pmvs)
-    print Chain.Render(tailStages[0],logPath)
+    print Chain.Render(tailStages[0],"persistence.txt")
     
     
 if __name__=="__main__":
-    
-    #sfmSbaChainProgrammatic("sbaChain.dat")
-    sfmChainProgrammatic("sfmChain.dat")
-    #sfmChainPersist("sfmChain.dat")
 
+    imagePath = os.path.abspath("../Datasets/ET")
+    sfmChainBuild("sfmChain.dat", imagePath)
+    sfmChainRestoreRender("sfmChain.dat")
 
--- code/Testing/sfm.py
+++ code/Testing/sfm.py
@@ -4,25 +4,19 @@ sys.path.append(os.path.abspath("."))
 import Chain # Chain must be imported first, requirement of registry
 import Sources, FeatureExtraction, FeatureMatch, BundleAdjustment, Cluster
 
-# path to images
+# path to images / PMVS
 imagePath = os.path.abspath("../Datasets/ET")
-
-# PMVS path
 pmvsPath = os.path.join(imagePath,"pmvs")
-
-# log file / whether to parse keypoint descriptors
-logPath = "log.txt"
-parseKDF = False
 
 # build chain
 imageSource = Sources.ImageSource(imagePath, "jpg")
-sift = FeatureExtraction.Sift(imageSource, parseKDF, "SiftHess", forceRun=True)
-keyMatch = FeatureMatch.KeyMatch(sift, parseKDF, "KeyMatchFull", forceRun=True)
-bundler = BundleAdjustment.Bundler([keyMatch, imageSource], forceRun=True)
+sift = FeatureExtraction.Sift(imageSource, False, "SiftHess")
+keyMatch = FeatureMatch.KeyMatch(sift, False, "KeyMatchFull")
+bundler = BundleAdjustment.Bundler([keyMatch, imageSource])
 radialUndistort = Cluster.RadialUndistort([bundler, imageSource])
 prepCmvsPmvs = Cluster.PrepCmvsPmvs(radialUndistort, pmvsPath)
 cmvs = Cluster.CMVS(prepCmvsPmvs)
 pmvs = Cluster.PMVS(cmvs)
 
 # render chain
-print Chain.Render(pmvs, "sfmTest.txt")
+print Chain.Render(pmvs, "sfm.txt")
