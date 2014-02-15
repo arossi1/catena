4a7da2268c333ebfd1ee769994c4aa8eb5231696
arossi1@gmail.com <arossi1@gmail.com@741b4108-eae3-c65b-5bbe-89a607054baa>
2014-02-15 00:32:19 +0000

    added increment property

--- code/Sources/ImageSubset.py
+++ code/Sources/ImageSubset.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012, Adam J. Rossi. All rights reserved. See README for licensing details.
+# Copyright (c) 2014, Adam J. Rossi. All rights reserved. See README for licensing details.
 import Chain
 import Common
 import random
@@ -7,16 +7,18 @@ class ImageSubset(Chain.StageBase):
 
     def __init__(self, inputStage=None, 
                  maxOutputImages=0, 
-                 startIndex=0):
+                 startIndex=0,
+                 increment=1):
         Chain.StageBase.__init__(self,
                                  inputStage,
                                  "Takes a subset of the source of images",
                                  {"Max Images":"Maximum images in the output set",
-                                  "Start Index":"Starting index of subset"})
+                                  "Start Index":"Starting index of subset",
+                                  "Increment":"Index increment"})
         
         self._properties["Max Images"] = maxOutputImages
         self._properties["Start Index"] = startIndex
-
+        self._properties["Increment"] = increment
 
     def GetInputInterface(self):
         return {"images":(0,Common.sfmImages)}
@@ -30,13 +32,12 @@ class ImageSubset(Chain.StageBase):
         self.StartProcess()
         
         imageList = images.GetImages()
+        
+        imageList = imageList[self._properties["Start Index"]::max([self._properties["Increment"],1])]
 
         if (self._properties["Max Images"]>0):
-            imageList = imageList[self._properties["Start Index"]:
-                                  self._properties["Start Index"]+self._properties["Max Images"]]
-        else:
-            imageList = imageList[self._properties["Start Index"]:]
-
+            imageList = imageList[:self._properties["Max Images"]]
+            
         images = Common.sfmImages(images.GetPath(),
                                   images.GetExtension(), 
                                   imageList,
