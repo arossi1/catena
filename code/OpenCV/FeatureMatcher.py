0c9087fff4bf9d183425e57bf81b2143b10c844b
arossi <Adam.Rossi@harris.com>
2019-08-23 15:53:59 -0400

    import updates for package

--- code/OpenCV/FeatureMatcher.py
+++ code/OpenCV/FeatureMatcher.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2014, Adam J. Rossi. All rights reserved. See README for licensing details.
-import Chain, Common
+from catena import Chain, Common, FeatureMatch
 import Types
-import FeatureMatch
 import os, string
 import numpy, cv2
 
