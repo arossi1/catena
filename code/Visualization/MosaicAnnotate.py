e086c6979334b09c8c91c7fa35485019adadcb4c
arossi <Adam.Rossi@harris.com>
2019-08-23 16:10:15 -0400

    import updates for package

--- code/Visualization/MosaicAnnotate.py
+++ code/Visualization/MosaicAnnotate.py
@@ -47,94 +47,95 @@ def drawLabel(iMosaic, label, org):
                 thickness=5, lineType=cv.CV_AA, bottomLeftOrigin=False)
     
 ###############################################################################
-SPACING = 50
-LABELS = True
-FEATURES = True
-CORRESPONDENCES = True
-
-IMAGE_PATH = r"C:\images"
-
-#imageGrid = [[0,1,2],
-#             [3,4,5],
-#             [6,7,8]]
-imageGrid = [[0,1],
-             [2,3]]
-
-########################################
-images=[]
-rowHeights=[]
-
-mosaicWidth=0;mosaicHeight=0;mosaicBands=0
-
-for row in imageGrid:
-    imageRow = []
-    rowWidth=0;rowHeight=0
-    for imageIndex in row:
-        im = readImage(IMAGE_PATH,imageIndex)
-        rowWidth+=im.shape[1]
-        rowHeight=max([rowHeight,im.shape[0]])
-        mosaicBands=max([mosaicBands,im.shape[2]])
-        imageRow.append(im)
-    rowWidth += (len(row)-1)*SPACING
-    mosaicWidth = max([rowWidth,mosaicWidth])
-    mosaicHeight+=rowHeight
-    rowHeights.append(rowHeight)
-    images.append(imageRow)
-mosaicHeight += (len(imageGrid)-1)*SPACING        
-
-iMosaic = numpy.zeros((mosaicHeight,mosaicWidth,mosaicBands), numpy.uint8)
-
-# create mosaic
-dy=0
-for r,imageRow in enumerate(images):
-    dx=0
-    for im in imageRow:
-        iMosaic[dy:dy+im.shape[0], dx:dx+im.shape[1], :im.shape[2]] = im
-        dx+=im.shape[1]+SPACING
-    dy+=rowHeights[r]+SPACING
+if __name__=="__main__":
+    SPACING = 50
+    LABELS = True
+    FEATURES = True
+    CORRESPONDENCES = True
     
+    IMAGE_PATH = r"C:\images"
     
+    #imageGrid = [[0,1,2],
+    #             [3,4,5],
+    #             [6,7,8]]
+    imageGrid = [[0,1],
+                 [2,3]]
     
-if (CORRESPONDENCES or LABELS or FEATURES):
+    ########################################
+    images=[]
+    rowHeights=[]
+    
+    mosaicWidth=0;mosaicHeight=0;mosaicBands=0
+    
+    for row in imageGrid:
+        imageRow = []
+        rowWidth=0;rowHeight=0
+        for imageIndex in row:
+            im = readImage(IMAGE_PATH,imageIndex)
+            rowWidth+=im.shape[1]
+            rowHeight=max([rowHeight,im.shape[0]])
+            mosaicBands=max([mosaicBands,im.shape[2]])
+            imageRow.append(im)
+        rowWidth += (len(row)-1)*SPACING
+        mosaicWidth = max([rowWidth,mosaicWidth])
+        mosaicHeight+=rowHeight
+        rowHeights.append(rowHeight)
+        images.append(imageRow)
+    mosaicHeight += (len(imageGrid)-1)*SPACING        
+    
+    iMosaic = numpy.zeros((mosaicHeight,mosaicWidth,mosaicBands), numpy.uint8)
+    
+    # create mosaic
     dy=0
-    for r in range(len(images)):
+    for r,imageRow in enumerate(images):
         dx=0
-        for c in range(len(images[r])):
-            refIm = images[r][c]
-            
-            if (LABELS):
-                drawLabel(iMosaic, str(imageGrid[r][c]), (dx+(refIm.shape[1]/2), dy+(refIm.shape[0]/2)))
-            
-            if (CORRESPONDENCES or FEATURES):
-                if (c+1<len(images[r])):
-                    testIm = images[r][c+1]
-                    testdx = dx+refIm.shape[1]+SPACING
-                    
-                    tiepoints = tps(IMAGE_PATH,imageGrid[r][c],imageGrid[r][c+1])
-                    
-                    for i,tp in enumerate(tiepoints):
-                        #if ((i%10)==0):
-                            x = (tp[1][0]+dx, tp[1][1]+dy)
-                            y = (tp[0][0]+testdx, tp[0][1]+dy)
-                            if (CORRESPONDENCES):   drawCorrespondence(iMosaic, x,y)
-                            if (FEATURES):          drawFeature(iMosaic, x,y)
-            
-                if (r+1<len(images)):
-                    testIm = images[r+1][c]
-                    testdy = dy+refIm.shape[0]+SPACING
-                    
-                    tiepoints = tps(IMAGE_PATH,imageGrid[r][c],imageGrid[r+1][c])
-                    
-                    for i,tp in enumerate(tiepoints):
-                        #if ((i%10)==0):
-                            x = (tp[1][0]+dx, tp[1][1]+dy)
-                            y = (tp[0][0]+dx, tp[0][1]+testdy)
-                            if (CORRESPONDENCES):   drawCorrespondence(iMosaic, x,y)
-                            if (FEATURES):          drawFeature(iMosaic, x,y)
-                            
-            dx+=refIm.shape[1]+SPACING
+        for im in imageRow:
+            iMosaic[dy:dy+im.shape[0], dx:dx+im.shape[1], :im.shape[2]] = im
+            dx+=im.shape[1]+SPACING
         dy+=rowHeights[r]+SPACING
-
-
-cv2.imwrite(os.path.join(IMAGE_PATH, "annotatedMosaic.png"), iMosaic)
-
+        
+        
+        
+    if (CORRESPONDENCES or LABELS or FEATURES):
+        dy=0
+        for r in range(len(images)):
+            dx=0
+            for c in range(len(images[r])):
+                refIm = images[r][c]
+                
+                if (LABELS):
+                    drawLabel(iMosaic, str(imageGrid[r][c]), (dx+(refIm.shape[1]/2), dy+(refIm.shape[0]/2)))
+                
+                if (CORRESPONDENCES or FEATURES):
+                    if (c+1<len(images[r])):
+                        testIm = images[r][c+1]
+                        testdx = dx+refIm.shape[1]+SPACING
+                        
+                        tiepoints = tps(IMAGE_PATH,imageGrid[r][c],imageGrid[r][c+1])
+                        
+                        for i,tp in enumerate(tiepoints):
+                            #if ((i%10)==0):
+                                x = (tp[1][0]+dx, tp[1][1]+dy)
+                                y = (tp[0][0]+testdx, tp[0][1]+dy)
+                                if (CORRESPONDENCES):   drawCorrespondence(iMosaic, x,y)
+                                if (FEATURES):          drawFeature(iMosaic, x,y)
+                
+                    if (r+1<len(images)):
+                        testIm = images[r+1][c]
+                        testdy = dy+refIm.shape[0]+SPACING
+                        
+                        tiepoints = tps(IMAGE_PATH,imageGrid[r][c],imageGrid[r+1][c])
+                        
+                        for i,tp in enumerate(tiepoints):
+                            #if ((i%10)==0):
+                                x = (tp[1][0]+dx, tp[1][1]+dy)
+                                y = (tp[0][0]+dx, tp[0][1]+testdy)
+                                if (CORRESPONDENCES):   drawCorrespondence(iMosaic, x,y)
+                                if (FEATURES):          drawFeature(iMosaic, x,y)
+                                
+                dx+=refIm.shape[1]+SPACING
+            dy+=rowHeights[r]+SPACING
+    
+    
+    cv2.imwrite(os.path.join(IMAGE_PATH, "annotatedMosaic.png"), iMosaic)
+    
