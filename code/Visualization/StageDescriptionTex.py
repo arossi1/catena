63b46fe70559ace142adb7b59be93e29f747c53a
arossi <Adam.Rossi@harris.com>
2019-08-23 16:20:49 -0400

    import updates for package

--- code/Visualization/StageDescriptionTex.py
+++ code/Visualization/StageDescriptionTex.py
@@ -8,24 +8,25 @@ def typeString(t):
     return str(t)[7:-2]
 
 
-reg = Chain.StageRegistry.registry
-
-print r"\begin{enumerate}"
-for package in reg.GetPackages():
-    stages = reg.GetStages(package)
-    if (len(stages)>0):
-        print r"  \item %s" % package
-        print r"    \begin{description}"
-        for stage in stages:
-            print r"      \item[%s] %s" % (stage, reg.GetStageDescription(package,stage))
-            pMap = reg.GetStagePropertyMap(package, stage)
-
-            if (len(pMap)>0):
-                print r"        \begin{description}"
-                for property in sorted(reg.GetStagePropertyMap(package, stage).keys()):
-                    print "          \\item[%s] \\hfill \\\\ \n            %s (%s)" % (property, 
-                                             reg.GetStagePropertyDescription(package,stage,property).replace("{","\\{").replace("}","\\}"), 
-                                             typeString(reg.GetStagePropertyType(package,stage,property)))
-                print r"        \end{description}"
-        print r"    \end{description}"
-print r"\end{enumerate}"
+if __name__=="__main__":
+    reg = Chain.StageRegistry.registry
+    
+    print r"\begin{enumerate}"
+    for package in reg.GetPackages():
+        stages = reg.GetStages(package)
+        if (len(stages)>0):
+            print r"  \item %s" % package
+            print r"    \begin{description}"
+            for stage in stages:
+                print r"      \item[%s] %s" % (stage, reg.GetStageDescription(package,stage))
+                pMap = reg.GetStagePropertyMap(package, stage)
+    
+                if (len(pMap)>0):
+                    print r"        \begin{description}"
+                    for property in sorted(reg.GetStagePropertyMap(package, stage).keys()):
+                        print "          \\item[%s] \\hfill \\\\ \n            %s (%s)" % (property, 
+                                                 reg.GetStagePropertyDescription(package,stage,property).replace("{","\\{").replace("}","\\}"), 
+                                                 typeString(reg.GetStagePropertyType(package,stage,property)))
+                    print r"        \end{description}"
+            print r"    \end{description}"
+    print r"\end{enumerate}"
--- code/Visualization/StageDescriptionTexTable.py
+++ code/Visualization/StageDescriptionTexTable.py
@@ -6,45 +6,47 @@ from catena import Chain
 def typeString(t):
     return str(t)[7:-2]
 
-reg = Chain.StageRegistry.registry
 
-
-thesis = True
-
-for package in reg.GetPackages():
-
-    stages = reg.GetStages(package)
-    if (len(stages)>0):
-        print r"\begin{landscape}"
-        if (thesis):    print r"\begin{longtable}{P{5cm} P{1cm} P{2.5cm} P{10cm}}"
-        else:           print r"\begin{longtable}{P{5cm} P{1cm} P{2.5cm} P{13cm}}"
-
-        print r"\caption{Auto-documentation for \textbf{%s} package} \\"%package
-
-        print r"\toprule"
-        print r"\multicolumn{4}{l}{\LARGE{\textbf{%s}}} \\" % package
-        print r"\midrule"
-        
-        for stage in stages:
-            print r"\textbf{\Large{%s}} & " % stage
-            print r"\multicolumn{3}{P{14cm}}{\emph{\large{%s}}} \\ " % reg.GetStageDescription(package,stage)
+if __name__=="__main__":
+    reg = Chain.StageRegistry.registry
+    
+    
+    thesis = True
+    
+    for package in reg.GetPackages():
+    
+        stages = reg.GetStages(package)
+        if (len(stages)>0):
+            print r"\begin{landscape}"
+            if (thesis):    print r"\begin{longtable}{P{5cm} P{1cm} P{2.5cm} P{10cm}}"
+            else:           print r"\begin{longtable}{P{5cm} P{1cm} P{2.5cm} P{13cm}}"
+    
+            print r"\caption{Auto-documentation for \textbf{%s} package} \\"%package
+    
+            print r"\toprule"
+            print r"\multicolumn{4}{l}{\LARGE{\textbf{%s}}} \\" % package
+            print r"\midrule"
             
-            stageInstance = reg.CreateInstance(package, stage)
-            pMap = reg.GetStagePropertyMap(package, stage)
-            if (len(pMap)>0):
+            for stage in stages:
+                print r"\textbf{\Large{%s}} & " % stage
+                print r"\multicolumn{3}{P{14cm}}{\emph{\large{%s}}} \\ " % reg.GetStageDescription(package,stage)
+                
+                stageInstance = reg.CreateInstance(package, stage)
+                pMap = reg.GetStagePropertyMap(package, stage)
+                if (len(pMap)>0):
+                    print r"\cmidrule(l){1-4}"
+                    print r"\textbf{Property Name} & \textbf{Data Type} & \textbf{Default Value} & \textbf{Description}\\"
+                    print r"\cmidrule(l){1-4}"
+                    for prop in sorted(reg.GetStagePropertyMap(package, stage).keys()):
+                        typeStr = typeString(reg.GetStagePropertyType(package,stage,prop))
+                        propDesc = reg.GetStagePropertyDescription(package,stage,prop).replace("{","\\{").replace("}","\\}")
+                        defaultVal = str(stageInstance.GetProperty(prop))
+                        if ((typeStr=="dict") or (typeStr=="str" and len(defaultVal)==0)):
+                            defaultVal = "[empty]"
+                        isEnum = (propDesc.find("{")>=0 and propDesc.find("}")>=0)
+                        if (isEnum): typeStr="enum"                    
+                        print r"%s & %s & %s & %s\\" % (prop, typeStr, defaultVal, propDesc)
                 print r"\cmidrule(l){1-4}"
-                print r"\textbf{Property Name} & \textbf{Data Type} & \textbf{Default Value} & \textbf{Description}\\"
-                print r"\cmidrule(l){1-4}"
-                for prop in sorted(reg.GetStagePropertyMap(package, stage).keys()):
-                    typeStr = typeString(reg.GetStagePropertyType(package,stage,prop))
-                    propDesc = reg.GetStagePropertyDescription(package,stage,prop).replace("{","\\{").replace("}","\\}")
-                    defaultVal = str(stageInstance.GetProperty(prop))
-                    if ((typeStr=="dict") or (typeStr=="str" and len(defaultVal)==0)):
-                        defaultVal = "[empty]"
-                    isEnum = (propDesc.find("{")>=0 and propDesc.find("}")>=0)
-                    if (isEnum): typeStr="enum"                    
-                    print r"%s & %s & %s & %s\\" % (prop, typeStr, defaultVal, propDesc)
-            print r"\cmidrule(l){1-4}"
-
-        print r"\end{longtable}"
-        print r"\end{landscape}"
+    
+            print r"\end{longtable}"
+            print r"\end{landscape}"
